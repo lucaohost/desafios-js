c33c1b700cc64e02af5098ef82e5d9fe1067b69b
Lucas Reginatto <lucas.reginatto.de.lima@gmail.com>
2020-11-23 01:46:10 -0300

    Update resposta_questao_2.js

--- resposta_questao_2.js
+++ resposta_questao_2.js
@@ -27,7 +27,7 @@ com espaços vazios e com o length errado. Por exemplo:
 let a = [];
 a[0] = true;
 a[2] = true;
-console.log('Se eu iterar esse array com empty, pode ser um problema:');
+console.log('Se eu iterar esse array, ele possui um empty no meio, e isso pode gerar bugs:');
 console.log(a);
 console.log('O length está com valor ' + a.length + ', mas o array possui apenas 2 elementos.');
 
